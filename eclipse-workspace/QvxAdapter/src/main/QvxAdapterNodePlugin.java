2b7ae3cade7ad32ea5da08269f6ce235b0c19b2c
monicasangam <32145733+monicasangam@users.noreply.github.com>
2019-03-19 11:03:30 -0400

    Qvx Node Classes and Development

---
+++ eclipse-workspace/QvxAdapter/src/main/QvxAdapterNodePlugin.java
@@ -0,0 +1,64 @@
+/* @(#)$RCSfile$ 
+ * $Revision$ $Date$ $Author$
+ *
+ */
+package edu.njit.knime.adapter.nodes.qvx;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * This is the eclipse bundle activator.
+ * Note: KNIME node developers probably won't have to do anything in here, 
+ * as this class is only needed by the eclipse platform/plugin mechanism.
+ * If you want to move/rename this file, make sure to change the plugin.xml
+ * file in the project root directory accordingly.
+ *
+ * @author Simple Qvx Adapter to Read and Write 
+ */
+public class QvxAdapterNodePlugin extends Plugin {
+    // The shared instance.
+    private static QvxAdapterNodePlugin plugin;
+
+    /**
+     * The constructor.
+     */
+    public QvxAdapterNodePlugin() {
+        super();
+        plugin = this;
+    }
+
+    /**
+     * This method is called upon plug-in activation.
+     * 
+     * @param context The OSGI bundle context
+     * @throws Exception If this plugin could not be started
+     */
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+
+    }
+
+    /**
+     * This method is called when the plug-in is stopped.
+     * 
+     * @param context The OSGI bundle context
+     * @throws Exception If this plugin could not be stopped
+     */
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        super.stop(context);
+        plugin = null;
+    }
+
+    /**
+     * Returns the shared instance.
+     * 
+     * @return Singleton instance of the Plugin
+     */
+    public static QvxAdapterNodePlugin getDefault() {
+        return plugin;
+    }
+
+}
