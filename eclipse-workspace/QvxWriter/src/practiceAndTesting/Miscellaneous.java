3a38bf1f586a298aa67e99bc3d2059b5d5a4f8cb
Matthew Belanger <mbelanger401@gmail.com>
2019-04-04 03:46:16 -0400

    QvxReader prototype finished
    QvxReader prototype finished

---
+++ eclipse-workspace/QvxWriter/src/practiceAndTesting/Miscellaneous.java
@@ -0,0 +1,22 @@
+package practiceAndTesting;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class Miscellaneous {
+
+	public static void main(String[] argv) {
+		//Object[] objs = new Object[1];
+		//objs[0] = ByteBuffer.wrap(new byte[] {3,5}).order(ByteOrder.BIG_ENDIAN).getShort();
+		
+		Object c = new Double(3.4);
+		System.out.println(c.getClass());
+		//System.out.println(.getClass().cast(c));
+		//Syste
+
+	}
+	
+	public static Object test() {
+		return 4.6;
+	}
+}
