8101d3ebe42e6c9cd35915ddc3280fd43ed7f08c
Matthew Belanger <mbelanger401@gmail.com>
2019-04-22 22:21:40 -0400

    Descriptions added to Advanced Panel
    Descriptions added to Advanced Panel

--- eclipse-workspace/QvxWriter/src/edu/njit/qvxwriter/AdvancedPanel.java
+++ eclipse-workspace/QvxWriter/src/edu/njit/qvxwriter/AdvancedPanel.java
@@ -1,9 +1,13 @@
 package edu.njit.qvxwriter;
 
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.TitledBorder;
@@ -13,32 +17,93 @@ import org.knime.core.node.NodeSettingsRO;
 
 import edu.njit.qvxwriter.QvxWriterNodeSettings.Endianness;
 
-import static edu.njit.util.Component.radioPanel;
-
 class AdvancedPanel extends JPanel {
 
 	private final JPanel recordSeparatorPanel;
 	private final JCheckBox recordSeparatorCheckBox;
+	private final JLabel recordSeparatorDescription;
 	
 	private final JPanel endiannessPanel;
-	private final JRadioButton endianness_bigEndianButton;
-	private final JRadioButton endianness_littleEndianButton;
+	private final JRadioButton bigEndianButton;
+	private final JRadioButton littleEndianButton;
+	private final JLabel endiannessDescription;
 	
 	public AdvancedPanel() {
+		
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		//Record separator panel
 		recordSeparatorPanel = new JPanel();
+		recordSeparatorPanel.setLayout(new GridBagLayout());
         recordSeparatorPanel.setBorder(new TitledBorder("Record Separator"));
-        recordSeparatorPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        recordSeparatorCheckBox = new JCheckBox("Use Record Separator");
-        recordSeparatorPanel.add(recordSeparatorCheckBox);
         
-        endianness_littleEndianButton = new JRadioButton();
-        endianness_bigEndianButton = new JRadioButton();
-        endiannessPanel = radioPanel("Endianness",
-        	new String[] {Endianness.LITTLE_ENDIAN.toString(), Endianness.BIG_ENDIAN.toString()},
-        	endianness_littleEndianButton, endianness_bigEndianButton
-        );
-        endianness_littleEndianButton.setSelected(true);
+        GridBagConstraints gbc = new GridBagConstraints();
+        
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        
+        recordSeparatorDescription = new JLabel(
+            	"<html>Specifies if a record separator byte should be written before each record.</html>");
+        
+        recordSeparatorCheckBox = new JCheckBox("Use Record Separator");   
+        
+        
+        recordSeparatorPanel.add(recordSeparatorDescription, gbc);
+        
+        gbc.gridy += 1;
+        recordSeparatorPanel.add(recordSeparatorCheckBox, gbc);
+        
+        //Endianness panel
+        endiannessPanel = new JPanel();
+        endiannessPanel.setBorder(new TitledBorder("Endianness"));
+    	endiannessPanel.setLayout(new GridBagLayout());
+        littleEndianButton = new JRadioButton();
+        bigEndianButton = new JRadioButton();
+        littleEndianButton.setText(Endianness.LITTLE_ENDIAN.toString());
+        bigEndianButton.setText(Endianness.BIG_ENDIAN.toString());
+        littleEndianButton.setSelected(true);
+        ButtonGroup group = new ButtonGroup();
+        group.add(littleEndianButton);
+        group.add(bigEndianButton);
+        
+        endiannessDescription = new JLabel(
+        	"<html>Specifies the byte-order of the numerical data values within the qvx file.</html>");
+        
+        gbc = new GridBagConstraints();
+        
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
+        gbc.weightx = 0.15;
+        gbc.weighty = 0.3;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        endiannessPanel.add(littleEndianButton, gbc);
+        
+        gbc.gridwidth = 1;
+        gbc.gridx += 1;
+        endiannessPanel.add(bigEndianButton, gbc);
+        
+        JPanel filler = new JPanel();
+        gbc.weightx = 0.7;
+        gbc.gridwidth = 1;
+        gbc.gridx += 1;
+        endiannessPanel.add(filler, gbc);
+        
+        gbc.weightx = 1;
+        gbc.weighty = 0.2;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        endiannessPanel.add(endiannessDescription, gbc);
         
         add(recordSeparatorPanel);
         add(endiannessPanel);
@@ -48,9 +113,9 @@ class AdvancedPanel extends JPanel {
 		
 		//isBigEndian
 		Boolean isBigEndian = null;
-		if (endianness_littleEndianButton.isSelected()) {
+		if (littleEndianButton.isSelected()) {
 			isBigEndian = false;
-		}else if (endianness_bigEndianButton.isSelected()) {
+		}else if (bigEndianButton.isSelected()) {
 			isBigEndian = true;
 		}
 		settings.setIsBigEndian(isBigEndian);
@@ -76,9 +141,9 @@ class AdvancedPanel extends JPanel {
 		
 		//isBigEndian
 		if (isBigEndian) {
-			endianness_bigEndianButton.setSelected(true);
+			bigEndianButton.setSelected(true);
 		}else {
-			endianness_littleEndianButton.setSelected(true);
+			littleEndianButton.setSelected(true);
 		}
 		
 		//usesRecordSeparator
